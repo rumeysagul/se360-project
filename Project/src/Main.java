6a364e8f10007274853be18351bfbcafdf1bcefb
Senkara <yavuz._senkara@hotmail.com>
2025-12-02 02:35:02 +0300

    Update Main.java

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,8 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("saaasasasas");
-        System.out.println("asdqwdqwdqwsaqwd");
-        System.out.println("sasaassasa");
-        System.out.println("asdqwdqwdqwsaqwd");
+        System.out.println("MAİN");
     }
 }
